34c2473638d0195900a09c2470d30fdffa0d1615
esteban127 <30833442+esteban127@users.noreply.github.com>
2019-03-14 11:56:47 -0300

    SceneLoaderPiola

---
+++ MobileProyect/Assets/LoaderManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LoaderManager : MonoBehaviour {
+    public Slider slider;
+
+    private void Awake()
+    {
+        LoadSceneAsync();
+    }
+
+    public void LoadSceneAsync()
+    {
+        StartCoroutine(Loading(MySceneManager.Instance.SceneToLoad));
+    }
+    IEnumerator Loading(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        slider.value = asyncLoad.progress;
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+}
---
+++ MobileProyect/Assets/MySceneManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour {
+
+    string sceneToLoad;
+    public string SceneToLoad {get{return sceneToLoad; }}
+    static MySceneManager instance = new MySceneManager();
+    public static MySceneManager Instance { get { return instance; } }
+
+    private MySceneManager()
+    {
+
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        sceneToLoad = sceneName;
+        SceneManager.LoadScene("LoadingScreen", LoadSceneMode.Single);       
+    }
+    public void LoadSceneAsync()
+    {
+        StartCoroutine(Loading(sceneToLoad));
+    }
+    IEnumerator Loading (string sceneName)
+    {        
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        float progress = asyncLoad.progress;
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
