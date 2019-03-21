01ae1ee3e1d7f087c59eb8c0e577acb58e956b4e
esteban127 <30833442+esteban127@users.noreply.github.com>
2019-03-21 11:17:46 -0300

    Pase a 2018 y agre el coso del atlas

---
+++ MobileProyect/Assets/Scripts/AtlasManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class AtlasManager : MonoBehaviour
+{
+    enum Textures
+    {
+        P,
+        U,
+        T,
+        O
+    }
+    [SerializeField] Textures textureToUse;
+    [SerializeField] SpriteAtlas atlas;
+    SpriteRenderer spRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(textureToUse.ToString());
+        spRenderer = GetComponent<SpriteRenderer>();
+        spRenderer.sprite = atlas.GetSprite(textureToUse.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwapToNextTexture();
+        }
+    }
+    private void SwapToNextTexture()
+    {
+        textureToUse =(Textures)(((int)textureToUse + 1 ) % (Enum.GetValues(typeof(AtlasManager.Textures)).Length)) ;
+        Debug.Log(textureToUse);
+        spRenderer.sprite = atlas.GetSprite(textureToUse.ToString());
+    }
+}
---
+++ MobileProyect/Assets/Scripts/Tester.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+
+    public string sceneToLoad;
+    private void Start()
+    {
+        MySceneManager.Instance.LoadScene(sceneToLoad);
+    }
+}
