3da621be590a60f4ea60f1f36860305b8c1312cc
esteban127 <30833442+esteban127@users.noreply.github.com>
2019-03-28 11:45:10 -0300

    Cosito pa dibujar

---
+++ MobileProyect/Assets/ChangeColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+    [SerializeField] GameObject testSprite;
+
+    public void OnClick()
+    {
+        testSprite.GetComponent<SpriteRenderer>().color = GetComponent<Image>().color;
+    }
+}
---
+++ MobileProyect/Assets/Scripts/touchTestScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class touchTestScript : MonoBehaviour
+{
+    [SerializeField]GameObject testSprite;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        for (int i = 0; i < Input.touches.Length; i++)
+        {
+            Debug.Log("no me toques ahi sempai");
+            if (Input.touches[i].phase == TouchPhase.Moved)
+            {
+                GameObject instance = Instantiate(testSprite);               
+                Vector3 screenPoint = new Vector3(Input.touches[i].position.x, Input.touches[i].position.y, 10.0f);
+                instance.transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+            }
+            if (Input.touches[i].phase == TouchPhase.Began)
+            {
+                GameObject instance = Instantiate(testSprite);
+                Vector3 screenPoint = new Vector3(Input.touches[i].position.x, Input.touches[i].position.y, 10.0f);
+                instance.transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+            }
+        }
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            GameObject instance = Instantiate(testSprite);
+            Vector3 screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+            instance.transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+        }*/
+    }
+}
